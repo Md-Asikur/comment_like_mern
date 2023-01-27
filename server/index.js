f6103a98f5b207c1578b406601684503fe484a9b
Md-Asikur <asikurrahaman997@gamil.com>
2023-01-26 18:56:01 -0800

    v2

--- server/index.js
+++ server/index.js
@@ -37,10 +37,10 @@ app.use("/uploads", express.static("uploads"));
  // //hosting
  if (process.env.NODE_ENV === "production") {
    // Set static folder
-    app.use(express.static(path.join(__dirname, "./client/build")));
+    app.use(express.static(path.join(__dirname, "/client/build")));
 
     app.get("*", (req, res) => {
-      res.sendFile(path.resolve(__dirname, "./client/build/index.html"));
+      res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
     });
  }
 
