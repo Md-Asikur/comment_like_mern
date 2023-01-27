4d4fe857028491740a2706790d8a94507fa845c5
Md-Asikur <asikurrahaman997@gamil.com>
2023-01-26 19:48:32 -0800

    insa--allah

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,7 +1,7 @@
 const proxy = require("http-proxy-middleware");
-
+require("dotenv").config;
 module.exports = function(app) {
 
-    app.use(proxy("/api", { target: "https://asikurlikecommentmern.cyclic.app/" }));
+    app.use(proxy("/api", { target: process.env.REACT_APP_SERVER_URL }));
 
 };
--- server/index.js
+++ server/index.js
@@ -35,15 +35,18 @@ app.use("/uploads", express.static("uploads"));
 // Serve static assets if in production
 
  // //hosting
- if (process.env.NODE_ENV === "production") {
-   // Set static folder
-  app.use(express.static("./client/build"));
+//  if (process.env.NODE_ENV === "production") {
+//    // Set static folder
+//   app.use(express.static("./client/build"));
 
-  // index.html for all page routes
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./client/build/index.html"));
-  });
- }
+//   // index.html for all page routes
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "./client/build/index.html"));
+//   });
+//  }
+app.get("/", (req, res) => {
+  res.json("Alhamdulillah")
+})
 
 const port = process.env.PORT || 5000;
 
